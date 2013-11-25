2c0c96f01a6bb55cdf165b8071cb60f896f61605
pboyer <peter.b.boyer@gmail.com>
2013-11-24 19:33:04 -0500

    Implement Polyline class

--- src/geom/Polyline.js
+++ src/geom/Polyline.js
@@ -1,24 +1,17 @@
-verb.geom.Polyline = function(points) {
+verb.geom.Polyline = function( points ) {
 
+	this.setAll( {
+		"control_points": points ? points.slice(0) : []
+	});
 
+	var curve_props = this.nurbsRep();
+
+	verb.geom.NurbsCurve.call(this, curve_props.degree, curve_props.control_points, curve_props.weights, curve_props.knots );
 
 }.inherits(verb.geom.NurbsCurve);
 
 verb.geom.Polyline.prototype.nurbsRep = function(){
 
-	
-
-
-
-
-
-
-
-
-	return this.nurbsEngine.eval_sync( 'get_cone_surface', [ this.get("axis"), 
-																												 this.get("xaxis"), 
-																												 this.get("base"), 
-																												 this.get("height"), 
-																												 this.get("radius") ]);
+	return this.nurbsEngine.eval_sync( 'get_polyline_surface', [ this.get("control_points") ]);
 
 };
